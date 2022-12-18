4038fbbc645a36cdfd7db9c71052651bf7cf6f34
kevinsanders133 <fairytailetyp@gmail.com>
2022-12-18 13:28:19 +0100

    implement admin service

--- AdminService/index.js
+++ AdminService/index.js
@@ -12,6 +12,142 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.post('/adminService/isQueryExist', (req, res) => {
+    const email = req.body.email;
+
+    try {
+        db.query(queries.isQueryExist, [email], (err, result) => {
+            if (err) throw err;
+
+            let message = false;
+
+            if (result[0].count === 0) {
+                message = true;
+            }
+
+            res.send(message);
+        });
+    } catch(e) {
+        res.sendStatus(500);
+    }
+});
+
+app.post('/adminService/register', (req, res) => {
+    const name = req.body.name;
+    const surname = req.body.surname;
+    const email = req.body.email;
+    const pass = req.body.pass;
+
+    try {
+        db.query(queries.register, [name, surname, email, pass], (err, _) => {
+            if (err) throw err;
+    
+            res.sendStatus(200);
+        });
+    } catch(e) {
+        res.sendStatus(500);
+    }
+});
+
+app.post('/adminService/getCountQueries', (_, res) => {
+    try {
+        db.query(queries.getCountQueries, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+
+            res.send(result[0].count);
+        });
+    } catch(e) {
+        res.sendStatus(500);
+    }
+});
+
+app.post('/adminService/deleteQuery', (req, res) => {
+    const id = req.body.id;
+
+    try {
+        db.query(queries.deleteQuery, [id], (err, _) => {
+            if (err) throw err;
+
+            res.sendStatus(200);
+        });
+    } catch(e) {
+        res.sendStatus(500);
+    }
+});
+
+app.post('/adminService/transferQuery', async (req, res) => {
+    const id = req.body.id;
+    const rank = req.body.rank;
+    const position = req.body.position;
+
+    const res1 = await (new Promise((resolve, reject) => {
+        db.query('transferQuery1', [id], (err, result) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(result);
+        });
+    }))();
+
+    const email = res1[0].email;
+    const password = res1[0].password;
+    const name = res1[0].name;
+    const surname = res1[0].surname;
+
+    const res2 = await (new Promise((resolve, reject)=>{
+        db.query('transferQuery2', [email, name, surname, password, rank, position], (err, result)=>{
+            if (err) {
+                return reject(err);
+            }
+            return resolve(result);
+        });
+    }))();
+
+    const res3 = await (new Promise((resolve, reject)=>{
+        db.query('transferQuery3', [id], (err, result)=>{
+            if (err) {
+                return reject(err);
+            }
+            return resolve(result);
+        });
+    }))();
+
+    let result = false;
+    if (res2 && res3) {
+        result = true;
+    }
+    res.send(result);
+});
+
+app.post('/adminService/getQueries', (_, res) => {
+    try {
+        db.query(queries.getQueries, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+
+            res.send(result);
+        });
+    } catch(e) {
+        res.sendStatus(500);
+    }
+});
+
+app.post('/adminService/getQueryById', (req, res) => {
+    const id = req.body.id;
+
+    try {
+        db.query(queries.getQueryById, [id], (err, result) => {
+            if (err) throw err;
+            console.log(result[0]);
+
+            res.send(result[0]);
+        });
+    } catch(e) {
+        res.sendStatus(500);
+    }
+});
+
 server.listen(5000, () => {
     console.log('App "AdminService" is running on port 5000.');
 });
--- AdminService/queries.js
+++ AdminService/queries.js
@@ -1,5 +1,43 @@
 const queries = {
-    getUsers: `insert into test values (1, '2')`,
+    isQueryExist: `
+        SELECT COUNT(*) as count
+        FROM queries
+        WHERE email = ?
+    `,
+    register: `
+        INSERT INTO queries (name, surname, email, password)
+	    VALUES (?, ?, ?, MD5(?))
+    `,
+    getCountQueries: `
+        SELECT count(*) as count
+        FROM queries
+    `,
+    deleteQuery: `
+        DELETE FROM queries
+        WHERE id = ?
+    `,
+    transferQuery1: `
+        SELECT email, name, surname, password
+        FROM queries
+        WHERE id = ?
+    `,
+    transferQuery2: `
+        INSERT INTO accounts(email, name, surname, password, rank, position)
+        values(?, ?, ?, ?, ?, ?);
+    `,
+    transferQuery3: `
+        DELETE FROM queries
+        WHERE id = ?
+    `,
+    getQueries: `
+        SELECT id, email, name, surname, regist_date
+        FROM queries;
+    `,
+    getQueryById: `
+        SELECT email, name
+        FROM queries 
+        WHERE id = ?
+    `
 }
 
 module.exports = { queries };
